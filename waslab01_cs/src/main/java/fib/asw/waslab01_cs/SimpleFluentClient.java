541729883384f4c16f0bb97b4f18ad4559515643
Pau Gibert <pau.gibert@ludiumlab.com>
2023-09-11 17:29:05 +0200

    Request per a la tasca 5 afegida en el client java

--- waslab01_cs/src/main/java/fib/asw/waslab01_cs/SimpleFluentClient.java
+++ waslab01_cs/src/main/java/fib/asw/waslab01_cs/SimpleFluentClient.java
@@ -12,13 +12,14 @@ public class SimpleFluentClient {
 	public final static void main(String[] args) throws Exception {
   	
   	/* Insert code for Task #4 here */
-	System.out.println(Request.post(URI).bodyForm(Form.form().add("author",  "Abel").add("tweet_text", "Hola").build())
+	System.out.println(Request.post(URI).bodyForm(Form.form().add("action",  "create").add("author",  "Abel").add("tweet_text", "Hola").build())
 	.addHeader("Accept", "text/plain").execute().returnContent());
 	
   	System.out.println(Request.get(URI).addHeader("Accept", "text/plain").execute().returnContent());
   	
   	/* Insert code for Task #5 here */
-  	
+  	System.out.println(Request.post(URI).bodyForm(Form.form().add("action",  "delete").add("author",  "Abel").add("tweet_text", "Hola").build())
+  	.addHeader("Accept", "text/plain").execute().returnContent());
   	
   }
 }
